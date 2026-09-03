391096c693808e56b059cdecc173e7f21e139372
xxxleshan <vova23122003@gmail.com>
2026-09-03 11:47:05 +0400

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\BookingController;
 Route::get('/', function () {
     return view('landing');
 });
-
+Route::get('/main', function () {
+    return view('main');
+});
 Route::post('/booking', [BookingController::class, 'store'])->name('booking.store');
 Route::get('/admin/bookings', [BookingController::class, 'index'])->name('admin.bookings');
 
